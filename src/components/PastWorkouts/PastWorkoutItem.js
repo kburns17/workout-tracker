ff535fe6796ae3727829ea8555a8d41e851fede3
Kerry Burns <kerryalanburns@gmail.com>
2018-05-04 14:03:48 -0500

    branching before demo

--- src/components/PastWorkouts/PastWorkoutItem.js
+++ src/components/PastWorkouts/PastWorkoutItem.js
@@ -8,15 +8,45 @@ const mapStateToProps = reduxState =>({
 });
 
 class PastWorkoutItem extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            editMode: false,
 
-    
+            workoutInputs: {
+               exercise: this.props.workout.exercise,
+               weight: this.props.workout.weight,
+               sets: this.props.workout.sets,
+               reps: this.props.workout.reps,
+               length: this.props.workout.length,
+               details: this.props.workout.details,
+            }
+        }
+    }
+
+
+    updateWorkout = () =>{
+        this.setState({
+            editMode: false, 
+        })
+        this.props.dispatch({
+            type: '',
+            payload: this.state.workoutInputs
+        })
+    }
 
 
 
+    handleEditClick =(event)=>{
+        this.setState({
+            editMode: true
+        })
+    }
 
     handleDeleteClick = (event)=>{
         this.props.deleteWorkout(this.props.workout)
     }
+
     handleFavoriteClick = (event)=>{
         this.props.favoriteWorkout(this.props.workout)
     }
@@ -25,7 +55,7 @@ class PastWorkoutItem extends Component {
     render(){
 
 
-        return(<div key={this.props.workout.id}><h3>{this.props.workout.exercise}</h3><p>{this.props.workout.weight}</p>
+        return(<div><h3>{this.props.workout.exercise}</h3><p>{this.props.workout.weight}</p>
             <p>{this.props.workout.sets}</p><p>{this.props.workout.reps}</p><p>{this.props.workout.length}</p>
             <p>{this.props.workout.details}{this.props.workout.favorite}</p>
             <button onClick={this.handleDeleteClick}>Remove</button>
