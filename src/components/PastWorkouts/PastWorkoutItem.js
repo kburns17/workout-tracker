75d3a7585618d82b5d7f2e2e4e50d9cc2fb3f12a
Kerry Burns <kerryalanburns@gmail.com>
2018-05-04 11:33:20 -0500

    starting edit

--- src/components/PastWorkouts/PastWorkoutItem.js
+++ src/components/PastWorkouts/PastWorkoutItem.js
@@ -9,10 +9,14 @@ const mapStateToProps = reduxState =>({
 
 class PastWorkoutItem extends Component {
 
+    
+
+
+
+
     handleDeleteClick = (event)=>{
         this.props.deleteWorkout(this.props.workout)
     }
-
     handleFavoriteClick = (event)=>{
         this.props.favoriteWorkout(this.props.workout)
     }
@@ -25,7 +29,8 @@ class PastWorkoutItem extends Component {
             <p>{this.props.workout.sets}</p><p>{this.props.workout.reps}</p><p>{this.props.workout.length}</p>
             <p>{this.props.workout.details}{this.props.workout.favorite}</p>
             <button onClick={this.handleDeleteClick}>Remove</button>
-            <button onClick={this.handleFavoriteClick}>Favorite</button></div>)
+            <button onClick={this.handleFavoriteClick}>Favorite</button>
+            <button onClick={this.handleEditClick}>Edit</button></div>)
             }
 
 }
